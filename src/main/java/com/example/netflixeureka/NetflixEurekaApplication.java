00d2798efc1820a100d426f54e8a80cb879199cd
ojAntoni <zezyulin90@gmail.com>
2023-07-21 17:25:24 +0300

    adding Spring Security support

--- src/main/java/com/example/netflixeureka/NetflixEurekaApplication.java
+++ src/main/java/com/example/netflixeureka/NetflixEurekaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+
 public class NetflixEurekaApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/netflixeureka/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.netflixeureka;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user")
+                .password(bCryptPasswordEncoder.encode("simplepassword"))
+                .build());
+        return manager;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(r -> r.requestMatchers("/eureka/**").authenticated());
+        http.authorizeHttpRequests(r -> r.anyRequest().permitAll());
+        return http.build();
+    }
+}
